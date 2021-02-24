6f6c8e4bdbdb29c392536c97b5146de4cd4034f4
ewe <xxx212212@gapp.nthu.edu.tw>
2021-02-24 02:16:01 -0800

    add helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (void) {
+    // print hello world for 9 times
+    for (int i=1; i<10; ++i) {
+        cout << "hello world " << i << " times!\n";
+    }
+    return 0;
+}
